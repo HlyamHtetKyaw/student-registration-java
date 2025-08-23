8e82a2ce4f4bbbb6ce5e845e6864b672588f342c
hlyamhtetkyaw <hlan1559@gmail.com>
2025-08-24 02:31:27 +0630

    [FIX] admin set password auto updatedAt bypass

--- src/main/java/org/tutgi/student_registration/data/repositories/UserRepository.java
+++ src/main/java/org/tutgi/student_registration/data/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -23,4 +24,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 		    Page<User> findAllFiltered(@Param("role") RoleName role,
 		                               @Param("keyword") String keyword,
 		                               Pageable pageable);
+	 @Modifying
+	 @Query("UPDATE User u SET u.password = :password WHERE u.email = :email")
+	 void updatePasswordByEmail(@Param("email") String email, @Param("password") String password);
+	 
+	 boolean existsByEmail(String email);
 }
