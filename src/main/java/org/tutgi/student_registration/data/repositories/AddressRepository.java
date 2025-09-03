2c6ebf8846d751a6fc4b7f1b4ee9b5ee0902947d
hlyamhtetkyaw <hlan1559@gmail.com>
2025-09-04 00:26:32 +0630

    [ADD] patch update route for entrance form

--- src/main/java/org/tutgi/student_registration/data/repositories/AddressRepository.java
+++ src/main/java/org/tutgi/student_registration/data/repositories/AddressRepository.java
@@ -1,13 +1,14 @@
 package org.tutgi.student_registration.data.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.tutgi.student_registration.data.enums.EntityType;
 import org.tutgi.student_registration.data.models.personal.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-
     List<Address> findByEntityTypeAndEntityId(EntityType entityType, Long entityId);
+    Optional<Address> findByEntityId(Long entityId);
 }
 
