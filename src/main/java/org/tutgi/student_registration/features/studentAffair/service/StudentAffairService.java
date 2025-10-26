51c305b935676499f40d4bd758f38a1751cb747e
Hlyam_Htet_Kyaw <133634944+HlyamHtetKyaw@users.noreply.github.com>
2025-10-26 11:09:47 +0630

    Update StudentAffairService.java

--- src/main/java/org/tutgi/student_registration/features/studentAffair/service/StudentAffairService.java
+++ src/main/java/org/tutgi/student_registration/features/studentAffair/service/StudentAffairService.java
@@ -3,7 +3,9 @@ package org.tutgi.student_registration.features.studentAffair.service;
 import org.tutgi.student_registration.config.response.dto.ApiResponse;
 import org.tutgi.student_registration.features.finance.dto.request.RejectionRequest;
 import org.tutgi.student_registration.features.studentAffair.dto.request.StudentAffairVerificationRequest;
-
+import org.springframework.data.domain.Pageable;
+import org.tutgi.student_registration.features.finance.dto.response.SubmittedStudentResponse;
+import org.tutgi.student_registration.config.response.dto.PaginatedApiResponse;
 public interface StudentAffairService {
 	ApiResponse verifyStudentByStudentAffair(Long studentId,StudentAffairVerificationRequest request);
 	ApiResponse rejectStudentByStudentAffair(Long studentId,RejectionRequest request);
